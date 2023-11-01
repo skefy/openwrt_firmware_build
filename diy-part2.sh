6765b219141531e4faed49b991321097fb3afab6
Leon <283585796@qq.com>
2023-11-01 11:39:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,5 +33,5 @@ sed -i 's/^PKG_HASH:=.*/PKG_HASH:=78c9c7f3e049d555b68914b545224cb9fd8253b75fe797
 sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=5.10.1/' feeds/packages/net/v2ray-core/Makefile
 sed -i 's/^PKG_HASH:=.*/PKG_HASH:=dfa0f9d6d1297819567cedad525025d2a6db07a1553213f2ecb2de110918c07c/' feeds/packages/net/v2ray-core/Makefile
 
-sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=5.10.0/' feeds/packages/net/vv2ray-plugin/Makefile
-sed -i 's/^PKG_HASH:=.*/PKG_HASH:=2ff37018e8bb6e9208206e84b9b5be77ad3d9a9e62bf2078b871f4f7066a87b8/' feeds/packages/net/vv2ray-plugin/Makefile
+sed -i 's/^PKG_VERSION:=.*/PKG_VERSION:=5.10.0/' feeds/packages/net/v2ray-plugin/Makefile
+sed -i 's/^PKG_HASH:=.*/PKG_HASH:=2ff37018e8bb6e9208206e84b9b5be77ad3d9a9e62bf2078b871f4f7066a87b8/' feeds/packages/net/v2ray-plugin/Makefile
