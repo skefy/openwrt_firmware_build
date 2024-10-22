56337c4683efc80069123f95bcf1dab6a08366ad
Leon <283585796@qq.com>
2024-10-22 14:10:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,3 +17,7 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generat
 # chage curl 8.6.0 to 8.5.0
 # rm -rf feeds/packages/net/curl
 # git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
+
+# config git 
+git config --global user.name "Leon"
+git config --global user.email leon@gmail.com
